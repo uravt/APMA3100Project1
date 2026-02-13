b48f891af77a7c6e4793f40cdb99f65ee3f41301
axx2xu <axx2xu@virginia.edu>
2026-02-12 22:12:38 -0500

    f: added loop for p values, along with print statements for count by realization value, calculated expected value, and plot for each p

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 
 alpha = 0.1
 
-
 def run_iteration():
     prob = p
     cycle = 1
     while True:
         rand_val = random.uniform(0, 1)
+        print(rand_val)
+        
         if rand_val <= prob:
+            print(cycle)
             return cycle
         else:
             prob = min(1, prob + (cycle - 1) * alpha)
@@ -28,10 +30,18 @@ for p in [0.1, 0.2, 0.3, 0.4, 0.5]:
     generate_trials()
     df = pd.read_csv("output_p="+str(p)+".csv")
 
+    total = 0
+    counts = df["X"].value_counts().sort_index()  # Series indexed by X value
+    print("p: "+str(p))
+    print("\nCounts by realization value (X):")
+    for x_val, cnt in counts.items():
+        print(f"X = {x_val}: {cnt}")
+        total += x_val * cnt
+    print(f"\nExpected Value E[X]: {total / len(df)}")
 
-# histogram of results
-plt.hist(df["X"], bins=20)
-plt.xlabel("Trial Result")
-plt.ylabel("Frequency")
-plt.title("X")
-plt.show()
+    # histogram of results
+    plt.hist(df["X"], bins=20)
+    plt.xlabel("Realization Value (X)")
+    plt.ylabel("Frequency")
+    plt.title("Histogram of Realization Values (X)")
+    plt.show()
